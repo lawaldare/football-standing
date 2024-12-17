1e43957eb04a5a23459df01c0b6944c8c83caca2
Dare Lawal <lawaldare@gmail.com>
2024-12-17 14:30:37 +0000

    updates

--- src/app/components/standings/standings.component.ts
+++ src/app/components/standings/standings.component.ts
@@ -1,42 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { StandingService } from 'src/app/services/standing.service';
-import { Team } from 'src/app/models/team.model';
-import { Observable } from 'rxjs';
-import { MatTableModule } from '@angular/material/table';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component } from "@angular/core";
+import { StandingService } from "src/app/services/standing.service";
+import { MatTableModule } from "@angular/material/table";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @Component({
-  selector: 'app-standings',
-  templateUrl: './standings.component.html',
-  styleUrls: ['./standings.component.css'],
-  standalone: true,
+  selector: "app-standings",
+  templateUrl: "./standings.component.html",
+  styleUrls: ["./standings.component.css"],
   imports: [MatTableModule, MatSelectModule, MatFormFieldModule],
 })
 export class StandingsComponent {
   columnsToDisplay = [
-    'logo',
-    'name',
-    'playedGames',
-    'won',
-    'draw',
-    'lost',
-    'goalsFor',
-    'goalsAgainst',
-    'goalDifference',
-    'points',
+    "logo",
+    "name",
+    "playedGames",
+    "won",
+    "draw",
+    "lost",
+    "goalsFor",
+    "goalsAgainst",
+    "goalDifference",
+    "points",
   ];
 
-  // league = this.standingService.selectedLeague;
-  standings = this.standingService.standings;
+  public standings = this.standingService.standings;
 
   constructor(private standingService: StandingService) {}
 
-  getStandings(league: string) {
+  public getStandings(league: string) {
     this.standingService.selectLeague(league);
   }
 
-  selectYear(year: string) {
+  public selectYear(year: string) {
     this.standingService.selectYear(year);
   }
 }
--- src/app/services/standing.service.ts
+++ src/app/services/standing.service.ts
@@ -1,48 +1,46 @@
-import { Injectable, signal } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { toSignal, toObservable } from '@angular/core/rxjs-interop';
-import { map, switchMap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-import { combineLatest } from 'rxjs';
+import { computed, inject, Injectable, signal } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { rxResource } from "@angular/core/rxjs-interop";
+import { map } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class StandingService {
-  private readonly newAPI = 'https://v3.football.api-sports.io/standings';
+  private readonly newAPI = "https://v3.football.api-sports.io/standings";
+  private readonly http = inject(HttpClient);
 
-  selectedLeague = signal<string>('39');
-  selectedYear = signal<string>('2023');
+  private selectedLeague = signal<string>("39");
+  private selectedYear = signal<string>("2024");
 
-  constructor(private http: HttpClient) {}
-
-  private standings$ = combineLatest([
-    toObservable(this.selectedLeague),
-    toObservable(this.selectedYear),
-  ]).pipe(
-    switchMap(([league, year]) => {
+  private standingResource = rxResource({
+    request: () => ({
+      league: this.selectedLeague(),
+      year: this.selectedYear(),
+    }),
+    loader: ({ request }) => {
       const httpOptions = {
         headers: new HttpHeaders({
-          'x-rapidapi-host': 'v3.football.api-sports.io',
-          'x-rapidapi-key': environment.apiToken,
+          "x-rapidapi-host": "v3.football.api-sports.io",
+          "x-rapidapi-key": environment.apiToken,
         }),
       };
-      return this.http.get(
-        `${this.newAPI}?league=${league}&season=${year}`,
-        httpOptions
-      );
-    }),
-    map((response: any) => {
-      return response.response[0].league.standings[0];
-    })
-  );
+      return this.http
+        .get(
+          `${this.newAPI}?league=${request.league}&season=${request.year}`,
+          httpOptions
+        )
+        .pipe(map((response: any) => response.response[0].league.standings[0]));
+    },
+  });
 
-  standings = toSignal(this.standings$, { initialValue: [] });
+  public standings = computed(() => this.standingResource.value());
 
-  selectLeague(league: string) {
+  public selectLeague(league: string): void {
     this.selectedLeague.set(league);
   }
-  selectYear(year: string) {
+  public selectYear(year: string): void {
     this.selectedYear.set(year);
   }
 }
