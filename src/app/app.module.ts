e85af1792190991ce1a12c7db5933c09daeab7bf
Dare Lawal <lawaldare@gmail.com>
2024-12-17 13:27:06 +0000

    to latest v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,12 @@ import { AppComponent } from './app.component';
 import { StandingsComponent } from './components/standings/standings.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  HttpClientModule,
-  HttpClient,
-  HTTP_INTERCEPTORS,
-} from '@angular/common/http';
+import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { StandingService } from './services/standing.service';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    StandingsComponent,
-    HeaderComponent,
-  ],
-  providers: [HttpClient, StandingService],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        StandingsComponent,
+        HeaderComponent], providers: [HttpClient, StandingService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
